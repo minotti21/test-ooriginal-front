f5d6bb411421b9a73b395b9d057bb183aa5e7d63
Vinicius Minotti <vinicius.minotti21@gmail.com>
2024-11-16 20:17:58 -0300

    configuring url

--- src/api/qrCodeApi.ts
+++ src/api/qrCodeApi.ts
@@ -1,6 +1,6 @@
 import apiClient from ".";
 
-const baseURL = "http://localhost:8080/qrcode";
+const baseURL = "https://qrcode-api-554937473875.southamerica-east1.run.app/qrcode";
 
 const getQrCodes = () => {
   return apiClient.get(baseURL);
